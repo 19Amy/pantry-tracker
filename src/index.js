447d934c2426716c6f09f3590de766d567f65c18
Amy <amygathonii@gmail.com>
2024-08-01 20:02:51 +0300

    Made pop up appear and dissapear when buttons are clicked

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+//Create items list 
+const popupButton = document.getElementById('Add-item-btn');
+const overlay = document.getElementById('overlay');
+const popup = document.getElementById('popup');
+const closePopupButton = document.getElementById('closePopup');
+const closeIcon = document.getElementById('close-popup');
+
+//Reveals the add items form 
+function showPopUp(){
+  overlay.style.display = 'block';
+  popup.style.display = 'block';
+}
+
+function closePopUp(){
+  overlay.style.display = 'none';
+  popup.style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+popupButton.addEventListener('click', showPopUp);
+closePopupButton.addEventListener('click', closePopUp);
+closeIcon.addEventListener('click', closePopUp);
+
+
+
 //increment & decrement counter
 const increment = document.getElementById('increment');
 const decrement = document.getElementById('decrement');
