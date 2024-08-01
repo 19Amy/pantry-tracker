d349d259883d57adc4a74d26817731f6b8c5b795
Amy <amygathonii@gmail.com>
2024-08-01 21:33:52 +0300

    created an add items function-when save changes is clicked items are displayed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const ul = document.getElementById("list-group")
+const inputBox = document.getElementById('input-box');
+const saveButton = document.getElementById("save-btn")
 //Create items list 
 const popupButton = document.getElementById('Add-item-btn');
 const overlay = document.getElementById('overlay');
@@ -10,26 +13,49 @@ function showPopUp(){
   overlay.style.display = 'block';
   popup.style.display = 'block';
 }
-
+//closes the add items form
 function closePopUp(){
   overlay.style.display = 'none';
   popup.style.display = 'none';
 }
 
-
-
-
-
-
-
-
-
-
-
 popupButton.addEventListener('click', showPopUp);
 closePopupButton.addEventListener('click', closePopUp);
 closeIcon.addEventListener('click', closePopUp);
 
+//Add items
+function addItems(){
+  if(inputBox ===""){
+    alert("please add your item");
+  }else{
+    let li =document.createElement("li");
+    li.classList.add("list-group-item");
+    li.innerHTML = inputBox.value
+    ul.appendChild(li);
+
+    let div = document.createElement("div");
+    div.classList.add("counter");
+    li.appendChild(div);
+
+    let i = document.createElement("i");
+    i.innerHTML += '<i class="fa-solid fa-square-minus fa-lg"></i>';
+    i.id ='decrement';
+    div.appendChild(i);
+
+    let p = document.createElement("p");
+    p.id="number";
+    p.innerText=0
+    div.appendChild(p);
+
+    let ii = document.createElement("i");
+      ii.innerHTML += '<i class="fa-solid fa-square-plus fa-lg" id="increment"></i>';
+      ii.id ='incrementt';
+    div.appendChild(ii);
+  }
+}
+
+
+saveButton.addEventListener("click", addItems)
 
 
 //increment & decrement counter
